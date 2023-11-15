0d66c469117c7b86a5f29216c4807ae11ca17fe8
uday <uudhhay365@gmail.com>
2023-11-15 02:58:48 +0000

    Loops

---
+++ 7.forloop2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+for i in {1..10}
+do
+	echo $i
+done
+
+echo ++++++++++++++++++++++++++++++++++++++++++++
+
+for i in {1..10..2} #{start..end..increment}
+do
+        echo $i
+done
+
---
+++ 7.selectloop.sh
@@ -0,0 +1,4 @@
+select name in $1 $2 $3 $4
+do
+	echo $name selected
+done
